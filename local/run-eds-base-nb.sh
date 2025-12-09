a37a769f8c52ca46bdb09d5d9f9e972811edb38e
Nicolas-Delahaye <nicolas.delahaye-ext@aphp.fr>
2025-12-09 09:34:27 +0100

    Update run-eds-base-nb.sh

--- local/run-eds-base-nb.sh
+++ local/run-eds-base-nb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -p 8888:8888 -it localhost/jupyterlab:eds-base-notebook_ubuntu-24.04-local
+docker run \
+    -p 8888:8888 \
+    -it localhost/jupyterlab:eds-base-notebook_ubuntu-24.04-local
