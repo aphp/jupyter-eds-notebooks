4003ecb1d7ad583f1ea5f4b81ed01bbbdbf1064a
Nicolas DELAHAYE <nicolas.delahaye-ext@aphp.fr>
2025-12-09 09:22:52 +0100

    remove sudo call and mounting volume on running phase.

--- local/run-eds-base-nb.sh
+++ local/run-eds-base-nb.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-VOLUME=/mnt/home/jovyan 
-
-if [ ! -f $VOLUME ]; then
-    sudo mkdir -p $VOLUME
-    sudo chown $(id -u):$(id -g) -R $VOLUME
-fi
-
-sudo docker run -v $VOLUME:/home/jovyan -p 8888:8888 -it localhost/jupyterlab:eds-base-notebook_ubuntu-24.04-local 
+docker run -p 8888:8888 -it localhost/jupyterlab:eds-base-notebook_ubuntu-24.04-local
