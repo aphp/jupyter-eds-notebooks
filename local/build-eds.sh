b29d5632b3aa68d00720aeda3f7291923e6f2278
7066189 <nicolas.delahaye-ext@aphp.fr>
2026-04-10 18:25:20 +0200

    fix : name for r image

--- local/build-eds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build \
-    --build-arg EDS_BASE_NOTEBOOK_IMAGE=quay.io/jupyter/r-notebook:x86_64-ubuntu-24.04 \
-    -f eds-r-notebook/Dockerfile \
-    -t localhost/jupyterlab:eds-r-notebook_ubuntu-24.04-local .
