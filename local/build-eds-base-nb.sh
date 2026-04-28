74472d8bffda3651039adf1f90c363dd48679c59
7066189 <nicolas.delahaye-ext@aphp.fr>
2026-04-28 13:54:16 +0200

    fix build

--- local/build-eds-base-nb.sh
+++ local/build-eds-base-nb.sh
@@ -2,5 +2,5 @@
 
 docker build \
     --build-arg BASE_IMAGE=quay.io/jupyter/base-notebook:ubuntu-24.04 \
-    -f eds-base-notebook/Dockerfile \
+    -f eds-image-hardening/Dockerfile \
     -t localhost/jupyterlab:eds-base-notebook_ubuntu-24.04-local .
